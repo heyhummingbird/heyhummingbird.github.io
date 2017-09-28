6c66e45402b2ac4c370cbd3df2cfa652524eb3fa
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-09-28 17:01:32 +0800

    get rid of unnecessary files

--- vocabulary_test/vocabulary/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Word(models.Model):
-	prob = models.TextField(default='')
-	ans = models.TextField(default='')
-	all_count = models.PositiveIntegerField(default=0)
-	correct_count = models.PositiveIntegerField(default=0)
